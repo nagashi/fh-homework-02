ad430a71c013ead62967f8c302d81c73e205154f
nagashi <ceoriley@gmail.com>
2019-03-28 22:23:30 +0000

    Error with saving isbn

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,23 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all 
+    @books = Book.order(:title).all
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    Book.create(book_params)
+    redirect_to root_path
+  end
+
+ 
 end
+
+
+ private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :genre, :publisher, :types, :year, :isbn)
+  end
